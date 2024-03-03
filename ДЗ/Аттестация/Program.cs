3c78e2983cc4bccdf44b04863fdf0e35af20e85d
Anastasiia <nastyasttp@gmail.com>
2024-03-03 17:03:08 +0500

    Add task2

--- ДЗ/Аттестация/Program.cs
+++ ДЗ/Аттестация/Program.cs
@@ -20,4 +20,25 @@ void che(int m, int n)
     Console.Write($"{m}, ");
 }
 che(m+1,n);
+
 }
+
+
+
+//  Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
+//  m = 2, n = 3 -> A(m,n) = 29
+ 
+ int AkkermanFunc(int m, int n)
+ {
+    if(m == 0)
+    {
+        return n + 1;
+    }
+    if(m > 0 && n == 0)
+    {
+       return AkkermanFunc(m - 1, 1); 
+    }
+    return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+}
+
+Console.WriteLine(AkkermanFunc(3,2));
