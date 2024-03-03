00bee8994a29c9d80bf3846e4fb19acb457334e3
Anastasiia <nastyasttp@gmail.com>
2024-03-03 16:57:56 +0500

    Задача 1 добавила решение

---
+++ ДЗ/Аттестация/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+// M = 1; N = 5 -> 1, 2, 3, 4, 5
+// M = 4; N = 8 -> 4, 5, 6, 7, 8
+
+using System.Dynamic;
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void che(int m, int n)
+{
+    if (m > n)
+    return;
+    if (m % 2 == 0);
+{
+    Console.Write($"{m}, ");
+}
+che(m+1,n);
+
+}
